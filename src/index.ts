485bb89edb381951eccfc0e17cdac474d4b21458
Slavko Josipović <slavkoj6@gmail.com>
2023-03-28 22:20:21 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,3 @@ import Changelog from "./Changelog"
 import Release from "./Release"
 
 export { Change, Changelog, parser, Release }
-
-parser(`# ESound Changelog
-
-(15-03-2023 - 25-03-2023)
-
-## 4.4.1 - 28-03-2023
-
-(Mobile)
-
-### Added
-
-- Swiper in lists for items
-
-### Fixed
-
-- Sorting queue problem`)
